08eba3a1d26b2be8e24c8822e768169a6c10f874
BubbleBurstt <atlack.e@gmail.com>
2026-08-01 23:32:22 +0545

    Make OrderSection.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import OrderSection from "./components/OrderSection";
 
 const App = () => {
   return (
     <div className="relative w-screen h-screen">
       <HeroSection />
+      <OrderSection />
     </div>
   );
 };
